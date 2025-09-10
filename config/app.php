a7a45fe7d3212a63840a11a353397c1d2a87544f
AlloJohannie <84252212+AlloJohannie@users.noreply.github.com>
2025-09-10 11:20:31 -0400

    Création du dossier LANG

--- config/app.php
+++ config/app.php
@@ -78,11 +78,9 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
-
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
-
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'locale' => 'fr_CA',
+    'fallback_locale' => 'fr',
+    'faker_locale' => 'fr_CA',
 
     /*
     |--------------------------------------------------------------------------
