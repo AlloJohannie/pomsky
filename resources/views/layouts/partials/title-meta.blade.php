ba48e3bd90790085468b4398e2304d827dd27a01
AlloJohannie <84252212+AlloJohannie@users.noreply.github.com>
2026-04-07 09:23:18 -0400

    favicon

--- resources/views/layouts/partials/title-meta.blade.php
+++ resources/views/layouts/partials/title-meta.blade.php
@@ -39,6 +39,8 @@ pomsky canada,
 <meta name="twitter:image" content="{{ asset('images/logo/logo_large.jpg') }}">
 
 <!-- Favicon -->
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-192.png') }}">
-<link rel="icon" type="image/png" sizes="512x512" href="{{ asset('favicon-512.png') }}">
-<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+<link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+<link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}">
